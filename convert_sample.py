27c15739d85a5e04493ff546d68082a30e89599a
Anhad-Shrivastava <93284342+Anhad-Shrivastava@users.noreply.github.com>
2021-10-29 00:37:33 +0530

    Delete convert_sample.py

--- convert_sample.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    csv_file = pd.DataFrame(pd.read_csv("Address_data.csv", sep = ",", header = 0, index_col = False))
-    csv_file.to_json("Address_data.json", orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
