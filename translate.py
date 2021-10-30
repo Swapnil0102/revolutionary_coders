0b9935430af2a6bd2c115eadc1b41d2c603e096f
Anhad-Shrivastava <93284342+Anhad-Shrivastava@users.noreply.github.com>
2021-10-30 15:06:51 +0530

    Delete translate.py

--- translate.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-def translate(string):
-    return 0
-
-if __name__ == '__main__':
-    translator = Translator()
-    ar = translator.translate('مرحبا').text
-    print(ar)
